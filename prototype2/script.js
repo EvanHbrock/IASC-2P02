f3feab78e93f9d14c6762a373c21bb45edb4a84f
evanhbrock <>
2026-01-30 20:47:31 -0500

    Add Prototype 2

--- prototype2/script.js
+++ prototype2/script.js
@@ -29,7 +29,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 scene.add(camera)
-camera.position.set(0, 0, 5)
+camera.position.set(0, 2, 5)
 //renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas, 
@@ -43,14 +43,60 @@ controls.enableDamping = true
 
 /*MESHES*/
 //testsphere
-const sphereGeometry = new THREE.SphereGeometry(1)
+const sphereGeometry = new THREE.ConeGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-
+const coneRotation = {
+    rotationspeed: 0.5
+}
 scene.add(testSphere)
+
+//plane
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 50, 50)
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = Math.PI * 0.5
+scene.add(plane)
 
 //*****UI*****
 const ui = new dat.GUI()
+// ui object
+const uiObject = {
+    speed: 1,
+    Distance: 1
+}
+//plane ui
+const planefolder = ui.addFolder('plane')
+planefolder
+    .add( planeMaterial, 'wireframe' )
+    .name("toggle Wireframe")
+
+//test sphere ui
+const spherefolder = ui.addFolder('Sphere')
+spherefolder
+    .add(uiObject, 'speed')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Speed')
+
+spherefolder
+    .add(uiObject, 'Distance')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Distance')
+
+    spherefolder
+    .add(coneRotation, 'rotationspeed')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Rotation Speed')
 
 /*ANIMATION LOOP
 ****************/
@@ -64,6 +110,10 @@ const animation = () =>
         const elapsedTime = clock.getElapsedTime(
             console.log("elapsedTime")
         )
+        //rotate
+        testSphere.rotation.x = elapsedTime * coneRotation.rotationspeed
+        //ANimate sphere
+        testSphere.position.y = Math.sin(elapsedTime * uiObject.speed) * uiObject.Distance
         //renderer
         renderer.render(scene, camera)
         //request next frame
